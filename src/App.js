588b3724635960315f0c1c06c03c142494954b88
niranjan-sharma-s <niranjansharma1992@gmail.com>
2022-06-16 17:34:11 +0530

    chore added test case

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+const [ color , setColor] = useState('red')
+const [disable , setDisable] = useState(false)
+const bgColor = color === 'red' ? 'blue' : 'red'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button  style={{"backgroundColor":color}} onClick = {() => setColor(bgColor)} disabled={disable? true : false}>Click to Change Color to {bgColor}</button>
+      <input type="checkbox"  onClick={()=> setDisable(!disable)}/>
     </div>
+    
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen,fireEvent} from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("initial text and color", () => {
+  render(<App/>)
+const buttonText = screen.getByRole("button" ,{name:'Click to Change Color to blue'})
+expect(buttonText).toHaveStyle({backgroundColor : 'red'})
+
+fireEvent.click(buttonText)
+
+expect(buttonText).toHaveStyle({backgroundColor : 'blue'})
+
+expect(buttonText.textContent).toBe('Click to Change Color to red')
+
+expect(buttonText).toBeEnabled()
+})
+
+test('button enabled test', () => {
+  render(<App/>)
+  const checkBox = screen.getByRole('checkbox')
+  expect(checkBox).not.toBeChecked()
+  
+})
+
+test('disable button , checkbox checked', () => {
+  render(<App/>)
+  const button = screen.getByRole('button')
+  const checkBox = screen.getByRole('checkbox')
+
+  fireEvent.click(checkBox)
+  //expect(checkBox).toBeChecked() default checked , not feasible condition 
+  expect(button).toBeDisabled()
+
+  fireEvent.click(checkBox)
+  expect(button).toBeEnabled()
+})
