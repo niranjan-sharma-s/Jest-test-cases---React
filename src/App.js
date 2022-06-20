46b7d2d37c9afcd466c4536dbb1fb5b74ba5d865
niranjan-sharma-s <niranjansharma1992@gmail.com>
2022-06-20 11:55:31 +0530

    chore added test cases

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import {useState} from 'react'
-import './App.css';
+/* eslint-disable react/react-in-jsx-scope */
+import { useState } from "react";
+import "./App.css";
+
+export const camelCaseSpace = (str) => {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      newStr += " " + str[i];
+    } else {
+      newStr += str[i];
+    }
+  }
+  return newStr;
+};
+
+camelCaseSpace("red");
 
 function App() {
-const [ color , setColor] = useState('red')
-const [disable , setDisable] = useState(false)
-const bgColor = color === 'red' ? 'blue' : 'red'
+  const [color, setColor] = useState("red");
+  const [disable, setDisable] = useState(false);
+  const bgColor = color === "red" ? "blue" : "red";
+  // const bgColor = disable?'grey':'red'
 
   return (
-    <div className='App'>
-      <button  style={{"backgroundColor":color}} onClick = {() => setColor(bgColor)} disabled={disable? true : false}>Click to Change Color to {bgColor}</button>
-      <input type="checkbox"  onClick={()=> setDisable(!disable)}/>
+    <div className="App">
+      <button
+        style={{ backgroundColor: disable ? "grey" : color }}
+        onClick={() => setColor(bgColor)}
+        disabled={!!disable}
+      >
+        Click to Change Color to {bgColor}
+      </button>
+      <label htmlFor="disable-button-checkbox">disable button</label>
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        onClick={() => setDisable(!disable)}
+      />
     </div>
-    
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,36 +1,62 @@
-import {render, screen,fireEvent} from '@testing-library/react'
-import App from './App'
+/* eslint-disable no-undef */
+import { render, screen, fireEvent } from '@testing-library/react'
+import App, { camelCaseSpace } from './App'
 
-test("initial text and color", () => {
+test('initial text and color', () => {
   render(<App/>)
-const buttonText = screen.getByRole("button" ,{name:'Click to Change Color to blue'})
-expect(buttonText).toHaveStyle({backgroundColor : 'red'})
+  const buttonText = screen.getByRole('button', { name: 'Click to Change Color to blue' })
+  expect(buttonText).toHaveStyle({ backgroundColor: 'red' })
 
-fireEvent.click(buttonText)
+  fireEvent.click(buttonText)
 
-expect(buttonText).toHaveStyle({backgroundColor : 'blue'})
+  expect(buttonText).toHaveStyle({ backgroundColor: 'blue' })
 
-expect(buttonText.textContent).toBe('Click to Change Color to red')
+  expect(buttonText.textContent).toBe('Click to Change Color to red')
 
-expect(buttonText).toBeEnabled()
+  expect(buttonText).toBeEnabled()
 })
 
 test('button enabled test', () => {
   render(<App/>)
   const checkBox = screen.getByRole('checkbox')
   expect(checkBox).not.toBeChecked()
-  
 })
 
 test('disable button , checkbox checked', () => {
   render(<App/>)
   const button = screen.getByRole('button')
-  const checkBox = screen.getByRole('checkbox')
+  const checkBox = screen.getByRole('checkbox', { name: 'disable button' })
 
   fireEvent.click(checkBox)
-  //expect(checkBox).toBeChecked() default checked , not feasible condition 
+  // expect(checkBox).toBeChecked() default checked , not feasible condition
   expect(button).toBeDisabled()
 
   fireEvent.click(checkBox)
   expect(button).toBeEnabled()
+})
+
+test('grey button on disable', () => {
+  render(<App/>)
+  const button = screen.getByRole('button')
+  const checkBox = screen.getByRole('checkbox', { name: 'disable button' })
+
+  fireEvent.click(checkBox)
+  expect(button).toHaveStyle({ backgroundColor: 'grey' })
+
+  fireEvent.click(checkBox)
+  expect(button).toHaveStyle({ backgroundColor: 'red' })
+})
+
+describe('test camelCase function', () => {
+  test('convert camelCase with one word', () => {
+    expect(camelCaseSpace('red')).toBe('red')
+  })
+
+  test('camelcase with two words', () => {
+    expect(camelCaseSpace('redBlue')).toBe('red Blue')
+  })
+
+  test('camelcase wth three words', () => {
+    expect(camelCaseSpace('redBlueGreen')).toBe('red Blue Green')
+  })
 })
